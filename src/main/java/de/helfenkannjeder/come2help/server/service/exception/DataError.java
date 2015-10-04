b348ca11eff9c6e44b2801fa789746e1a6fa40b9
KrugT <KrugT@users.noreply.github.com>
2015-10-04 15:29:42 +0200

    add findbugs

--- src/main/java/de/helfenkannjeder/come2help/server/service/exception/DataError.java
+++ src/main/java/de/helfenkannjeder/come2help/server/service/exception/DataError.java
@@ -1,8 +1,12 @@
 package de.helfenkannjeder.come2help.server.service.exception;
 
-public class DataError {
-    public String code;
-    public String value;
+import java.io.Serializable;
+
+public class DataError implements Serializable {
+
+    private static final long serialVersionUID = -2300011506801088463L;
+    public final String code;
+    public final String value;
 
     public DataError(String code, String value) {
         this.code = code;
