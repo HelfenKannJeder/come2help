0ae19f264e6cfc7080b7524ea0d936e1fe962e49
Marius <marius-im@web.de>
2015-10-31 17:56:54 +0100

    introduce jsondoc to document the json api add OrganisationController

---
+++ src/main/java/de/helfenkannjeder/come2help/server/service/OrganisationService.java
@@ -0,0 +1,79 @@
+package de.helfenkannjeder.come2help.server.service;
+
+import com.google.common.collect.Lists;
+import de.helfenkannjeder.come2help.server.domain.Organisation;
+import de.helfenkannjeder.come2help.server.domain.repository.OrganisationRepository;
+import de.helfenkannjeder.come2help.server.service.exception.ConcurrentDeletedException;
+import de.helfenkannjeder.come2help.server.service.exception.DuplicateResourceException;
+import de.helfenkannjeder.come2help.server.service.exception.InvalidDataException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.lang.String.format;
+
+@Service
+public class OrganisationService {
+    private final OrganisationRepository organisationRepository;
+
+    @Autowired
+    public OrganisationService(OrganisationRepository organisationRepository) {
+        this.organisationRepository = organisationRepository;
+    }
+
+    public List<Organisation> findAll() {
+        return Lists.newArrayList(organisationRepository.findAll());
+    }
+
+    public Organisation findById(Long id) {
+        return organisationRepository.findOne(id);
+    }
+
+    public Organisation createOrganisation(Organisation organisation) {
+        if (organisation == null) {
+            return organisation;
+        }
+
+        Organisation tmp = organisationRepository.findByName(organisation.getName());
+        if (tmp != null) {
+            throw new DuplicateResourceException(format("An organisation with name %s already exists", organisation.getName()));
+        }
+
+        return organisationRepository.save(organisation);
+    }
+
+    public Organisation updateOrganisation(Organisation organisation) {
+        if (organisation == null) {
+            return null;
+        }
+
+        if (organisation.getId() == null) {
+            throw InvalidDataException.forSingleError("id.not.null", null);
+        }
+
+        Organisation tmp = organisationRepository.findOne(organisation.getId());
+        if (tmp == null) {
+            throw new ConcurrentDeletedException(organisation.getId());
+        }
+
+        return organisationRepository.save(organisation);
+    }
+
+    public void deleteOrganisationById(Long id) {
+        Organisation organisation = organisationRepository.findOne(id);
+        if (organisation == null) {
+            return;
+        }
+
+        organisationRepository.delete(id);
+    }
+
+    public void deleteOrganisation(Organisation organisation) {
+        if (organisation == null) {
+            return;
+        }
+
+        deleteOrganisationById(organisation.getId());
+    }
+}
