45949a813161943b705f48bdd904e4720caa96f7
Marius <marius-im@web.de>
2015-11-07 20:44:04 +0100

    enable password hashing and enrich address with lat and lgn

--- src/main/java/de/helfenkannjeder/come2help/server/service/OrganisationService.java
+++ src/main/java/de/helfenkannjeder/come2help/server/service/OrganisationService.java
@@ -1,11 +1,13 @@
 package de.helfenkannjeder.come2help.server.service;
 
 import com.google.common.collect.Lists;
+import de.helfenkannjeder.come2help.server.domain.Address;
 import de.helfenkannjeder.come2help.server.domain.Organisation;
 import de.helfenkannjeder.come2help.server.domain.repository.OrganisationRepository;
 import de.helfenkannjeder.come2help.server.service.exception.ConcurrentDeletedException;
 import de.helfenkannjeder.come2help.server.service.exception.DuplicateResourceException;
 import de.helfenkannjeder.come2help.server.service.exception.InvalidDataException;
+import de.helfenkannjeder.come2help.server.util.googleapi.GeoCodeCaller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -40,6 +42,8 @@ public class OrganisationService {
             throw new DuplicateResourceException(format("An organisation with name %s already exists", organisation.getName()));
         }
 
+        organisation = enrichInformation(organisation);
+
         return organisationRepository.save(organisation);
     }
 
@@ -57,9 +61,12 @@ public class OrganisationService {
             throw new ConcurrentDeletedException(organisation.getId());
         }
 
+        organisation = enrichInformation(organisation);
+
         return organisationRepository.save(organisation);
     }
 
+    //TODO should we really delete organisations or to be able to track historical stuff just soft-delete/deactivate them?
     public void deleteOrganisationById(Long id) {
         Organisation organisation = organisationRepository.findOne(id);
         if (organisation == null) {
@@ -75,5 +82,17 @@ public class OrganisationService {
         }
 
         deleteOrganisationById(organisation.getId());
+    }
+
+    /**
+     * enrich information which calculated ones (address: lat, lgn)
+     *
+     * @param organisation
+     * @return
+     */
+    private Organisation enrichInformation(Organisation organisation) {
+        Address enrichedAddress = GeoCodeCaller.enrichAddressWithLatAndLgn(organisation.getAddress());
+        organisation.setAddress(enrichedAddress);
+        return organisation;
     }
 }
