8d10741c389a394599e8e37e961777cb249cc9ed
KrugT <KrugT@users.noreply.github.com>
2015-11-23 23:33:29 +0100

    Remove whitespace from error message

--- src/main/java/de/helfenkannjeder/come2help/server/service/exception/ResourceNotFoundException.java
+++ src/main/java/de/helfenkannjeder/come2help/server/service/exception/ResourceNotFoundException.java
@@ -2,7 +2,9 @@ package de.helfenkannjeder.come2help.server.service.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
 
+    private static final long serialVersionUID = 4062618674483457639L;
+
     public ResourceNotFoundException(Object id) {
-        super(String.format("Object with ID %s has not been found ", id));
+        super(String.format("Object with ID %s has not been found", id));
     }
 }
