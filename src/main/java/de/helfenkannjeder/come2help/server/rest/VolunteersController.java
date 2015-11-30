0b9477891b1d713506752ae9a7d44562e79e225e
KrugT <KrugT@users.noreply.github.com>
2015-11-30 21:40:44 +0100

    close #22 introduced AbilityLinkDto which validates only the linking id

--- src/main/java/de/helfenkannjeder/come2help/server/rest/VolunteersController.java
+++ src/main/java/de/helfenkannjeder/come2help/server/rest/VolunteersController.java
@@ -1,17 +1,25 @@
 package de.helfenkannjeder.come2help.server.rest;
 
-import de.helfenkannjeder.come2help.server.domain.Coordinate;
-import de.helfenkannjeder.come2help.server.domain.Volunteer;
-import de.helfenkannjeder.come2help.server.rest.dto.VolunteerDto;
-import de.helfenkannjeder.come2help.server.service.VolunteersService;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+
+import de.helfenkannjeder.come2help.server.domain.Coordinate;
+import de.helfenkannjeder.come2help.server.domain.Volunteer;
+import de.helfenkannjeder.come2help.server.rest.dto.VolunteerDto;
+import de.helfenkannjeder.come2help.server.rest.dto.VolunteerResponseDto;
+import de.helfenkannjeder.come2help.server.service.VolunteersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/volunteers")
@@ -29,31 +37,31 @@ public class VolunteersController {
     @ResponseStatus(HttpStatus.OK)
     public List<VolunteerDto> getVolunteers(@RequestParam @NotNull Double latitude, @RequestParam @NotNull Double longitude, @RequestParam @NotNull Double distance) {
         List<Volunteer> volunteers = volunteersService.findAllInDistance(new Coordinate(latitude, longitude), distance);
-        return volunteers.stream().map(v -> VolunteerDto.createFullDto(v)).collect(Collectors.toList());
+        return volunteers.stream().map(VolunteerDto::createFullDto).collect(Collectors.toList());
     }
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public VolunteerDto createVolunteer(@Valid @RequestBody VolunteerDto volunteerDto) {
+    public VolunteerResponseDto createVolunteer(@Valid @RequestBody VolunteerDto volunteerDto) {
         Volunteer volunteer = VolunteerDto.createVolunteer(volunteerDto);
         Volunteer createdVolunteer = volunteersService.createVolunteer(volunteer);
-        return VolunteerDto.createFullDto(createdVolunteer);
+        return VolunteerResponseDto.createFullDto(createdVolunteer);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public VolunteerDto getVolunteerById(@PathVariable Long id) {
+    public VolunteerResponseDto getVolunteerById(@PathVariable Long id) {
         Volunteer volunteer = volunteersService.findById(id);
-        return VolunteerDto.createFullDto(volunteer);
+        return VolunteerResponseDto.createFullDto(volunteer);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.OK)
-    public VolunteerDto updateVolunteer(@NotNull @PathVariable Long id, @Valid @RequestBody VolunteerDto volunteerDto) {
+    public VolunteerResponseDto updateVolunteer(@NotNull @PathVariable Long id, @Valid @RequestBody VolunteerDto volunteerDto) {
         volunteerDto.setId(id);
         Volunteer volunteer = VolunteerDto.createVolunteer(volunteerDto);
         Volunteer updatedVolunteer = volunteersService.updateVolunteer(volunteer);
-        return VolunteerDto.createFullDto(updatedVolunteer);
+        return VolunteerResponseDto.createFullDto(updatedVolunteer);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
