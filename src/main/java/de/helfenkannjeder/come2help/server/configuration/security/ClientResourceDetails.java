fae219da0b14df2415c35e6cfbdf1dbe12f417b1
Andreas Eberle <email@andreas-eberle.com>
2015-12-19 18:18:08 +0100

    Reworked oauth2 authentication and added CustomOAuthAuthenticationProcessingFilter.java to handle authorization requests of satellizer.

--- src/main/java/de/helfenkannjeder/come2help/server/configuration/security/ClientResourceDetails.java
+++ src/main/java/de/helfenkannjeder/come2help/server/configuration/security/ClientResourceDetails.java
@@ -3,7 +3,9 @@ package de.helfenkannjeder.come2help.server.configuration.security;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ClientResourceDetails {
 
     private final OAuth2ProtectedResourceDetails client = new AuthorizationCodeResourceDetails();
--- src/main/java/de/helfenkannjeder/come2help/server/configuration/security/OAuth2ClientConfigurer.java
+++ src/main/java/de/helfenkannjeder/come2help/server/configuration/security/OAuth2ClientConfigurer.java
@@ -6,19 +6,14 @@ import de.helfenkannjeder.come2help.server.security.jwt.StatelessJwtAuthenticati
 import java.util.Arrays;
 import javax.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -29,7 +24,7 @@ import org.springframework.web.filter.CompositeFilter;
 public class OAuth2ClientConfigurer extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private OAuth2ClientContext oAuth2ClientContext;
+    private MappingJackson2HttpMessageConverter jsonMessageConverter;
 
     /**
      * Configure HttpSecurity. This includes:<br>
@@ -47,43 +42,32 @@ public class OAuth2ClientConfigurer extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .csrf().disable().headers().frameOptions().disable().and()
                 .antMatcher("/**").authorizeRequests()
-                /**/.antMatchers("/abilities", "/login/**", "/logoutWorked").permitAll()
+                /**/.antMatchers("/abilities", "/login/**").permitAll()
                 /**/.anyRequest().authenticated().and()
-                .exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint()).and()
-                .addFilterBefore(statelessJwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+                .exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint()).and();
+
+        httpSecurity.addFilterBefore(statelessJwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.addFilterBefore(createOAuth2Filter(), BasicAuthenticationFilter.class);
     }
 
-    private Filter ssoFilter() {
+    private Filter createOAuth2Filter() {
         CompositeFilter filter = new CompositeFilter();
         filter.setFilters(Arrays.asList(
-                createSsoFilter(facebook(), facebookSuccessHandler(), "/login/facebook"),
-                createSsoFilter(google(), googleSuccessHandler(), "/login/google"))
+                createOAuth2Filter(facebook(), facebookSuccessHandler(), "/login/facebook"),
+                createOAuth2Filter(google(), googleSuccessHandler(), "/login/google"))
         );
         return filter;
     }
 
-    private OAuth2ClientAuthenticationProcessingFilter createSsoFilter(ClientResourceDetails clientDetails, AuthenticationSuccessHandler successHandler, String path) {
-        OAuth2ClientAuthenticationProcessingFilter ssoFilter = new OAuth2ClientAuthenticationProcessingFilter(path);
-        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(clientDetails.getClient(), oAuth2ClientContext);
-        restTemplate.setAccessTokenProvider(authorizationCodeAccessTokenProvider());
-        ssoFilter.setRestTemplate(restTemplate);
-        ssoFilter.setTokenServices(new UserInfoTokenServices(clientDetails.getResource().getUserInfoUri(), clientDetails.getClient().getClientId()));
-        ssoFilter.setAuthenticationSuccessHandler(successHandler);
-        return ssoFilter;
+    private AbstractAuthenticationProcessingFilter createOAuth2Filter(ClientResourceDetails clientDetails, AuthenticationSuccessHandler successHandler, String path) {
+        CustomOAuthAuthenticationProcessingFilter oauthFilter = new CustomOAuthAuthenticationProcessingFilter(path, clientDetails, jsonMessageConverter);
+        oauthFilter.setAuthenticationSuccessHandler(successHandler);
+        return oauthFilter;
     }
 
     @Bean
     protected StatelessJwtAuthenticationFilter statelessJwtAuthenticationFilter() {
         return new StatelessJwtAuthenticationFilter();
-    }
-
-    @Bean // handles the redirect to facebook
-    public FilterRegistrationBean oAuth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(filter);
-        registration.setOrder(-100);
-        return registration;
     }
 
     @Bean
@@ -107,12 +91,4 @@ public class OAuth2ClientConfigurer extends WebSecurityConfigurerAdapter {
     protected AuthenticationSuccessHandler googleSuccessHandler() {
         return new GoogleSuccessHandler();
     }
-
-    @Bean
-    public AuthorizationCodeAccessTokenProvider authorizationCodeAccessTokenProvider() {
-        AuthorizationCodeAccessTokenProvider authorizationCodeAccessTokenProvider = new AuthorizationCodeAccessTokenProvider();
-        authorizationCodeAccessTokenProvider.setStateMandatory(false);
-        return authorizationCodeAccessTokenProvider;
-    }
-
 }
