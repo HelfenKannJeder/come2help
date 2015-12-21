e23cb87521f294aaba9275491b6c4a3cb3050970
KrugT <KrugT@users.noreply.github.com>
2015-12-21 03:15:54 +0100

    permit jsondoc access without authentication

--- src/main/java/de/helfenkannjeder/come2help/server/configuration/security/OAuth2ClientConfigurer.java
+++ src/main/java/de/helfenkannjeder/come2help/server/configuration/security/OAuth2ClientConfigurer.java
@@ -42,8 +42,8 @@ public class OAuth2ClientConfigurer extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .csrf().disable().headers().frameOptions().disable().and()
                 .antMatcher("/**").authorizeRequests()
-                /**/.antMatchers("/abilities", "/login/**").permitAll()
-                /**/.anyRequest().authenticated().and()
+                .antMatchers("/abilities", "/login/**", "/jsondoc/**", "/jsondoc-ui.html", "/webjars/jsondoc-ui-webjar/**").permitAll()
+                .anyRequest().authenticated().and()
                 .exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint()).and();
 
         httpSecurity.addFilterBefore(statelessJwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
