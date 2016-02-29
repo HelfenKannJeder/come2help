7a8d09ecdada8312bde9fe3ce0f007668f9ab52c
KrugT <KrugT@users.noreply.github.com>
2016-02-29 21:58:55 +0100

    Fix volunteer test, add user mock

--- src/test/java/de/helfenkannjeder/come2help/server/domain/VolunteerTest.java
+++ src/test/java/de/helfenkannjeder/come2help/server/domain/VolunteerTest.java
@@ -4,23 +4,31 @@ import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class VolunteerTest {
+
+    @Mock
+    private User user1;
 
     @Test
     public void testUpdate() {
         final Long testId1 = 2L;
         final List<Ability> testAbilities1 = Collections.singletonList(new Ability(1L, null, null));
 
-        Volunteer volunteer1 = new Volunteer().setId(testId1).setAbilities(testAbilities1);
+        Volunteer volunteer1 = new Volunteer().setId(testId1).setAbilities(testAbilities1).setUser(user1);
 
         final Long testId2 = 42L;
         final List<Ability> testAbilities2 = Collections.singletonList(new Ability(5L, null, null));
 
-        Volunteer volunteer2 = new Volunteer().setId(testId2).setAbilities(testAbilities2);
+        Volunteer volunteer2 = new Volunteer().setId(testId2).setAbilities(testAbilities2).setUser(user1);
 
         assertNotEquals(volunteer1, volunteer2);
 
@@ -31,5 +39,7 @@ public class VolunteerTest {
 
         volunteer2.setId(testId1);
         assertEquals(volunteer2, volunteer1);
+
+        verify(user1).update(user1);
     }
 }
