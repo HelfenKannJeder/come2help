1fcf6a1465d288174123ad5d058edf6c45592c1a
Marius <marius-im@web.de>
2016-03-05 15:59:28 +0100

    use greenmail to test mailservice

--- src/test/java/de/helfenkannjeder/come2help/server/util/mail/MailServiceTest.java
+++ src/test/java/de/helfenkannjeder/come2help/server/util/mail/MailServiceTest.java
@@ -1,5 +1,8 @@
 package de.helfenkannjeder.come2help.server.util.mail;
 
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetup;
 import de.helfenkannjeder.come2help.server.cucumber.configuration.TestApplicationConfiguration;
 import org.junit.After;
 import org.junit.Before;
@@ -8,36 +11,39 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.subethamail.wiser.Wiser;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- * based on http://blog.codeleak.pl/2014/09/testing-mail-code-in-spring-boot.html
+ * based on http://www.javavillage.in/greenmail-java-mail.php
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestApplicationConfiguration.class)
 @WebAppConfiguration
 public class MailServiceTest {
 
-    private Wiser wiser;
+
+   private GreenMail testSmtp;
 
     @Before
-    public void setUp() throws Exception {
-        wiser = new Wiser();
-        wiser.start();
+    public void testSmtpInit(){
+        ServerSetup setup = new ServerSetup(25, "localhost", "smtp");
+        testSmtp = new GreenMail(setup);
+        testSmtp.start();
     }
 
     @After
-    public void tearDown() throws Exception {
-        wiser.stop();
+    public void cleanup(){
+        testSmtp.stop();
     }
 
     @Test
     public void send() throws Exception {
         new MailService().send("someone@localhosts", "someone@localhost", "Lorem ipsum", "Lorem ipsum dolor sit amet [...]");
-        WiserAssertions.assertReceivedMessage(wiser)
-                .from("someone@localhosts")
-                .to("someone@localhost")
-                .withSubject("Lorem ipsum")
-                .withContent("Lorem ipsum dolor sit amet [...]");
+        javax.mail.Message[] messages = testSmtp.getReceivedMessages();
+        assertEquals(1, messages.length);
+        assertEquals("Lorem ipsum", messages[0].getSubject());
+        String body = GreenMailUtil.getBody(messages[0]).replaceAll("=\r?\n", "");
+        assertEquals("Lorem ipsum dolor sit amet [...]", body);
     }
 }
